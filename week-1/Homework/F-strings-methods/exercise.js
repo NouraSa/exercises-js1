9a1808a04bd4a5dd0e49de0292c8adb3163a0a37
Noura <oceannoura@gmail.com>
2020-08-17 16:07:40 +0200

    ex.F stringMethods_ done

--- week-1/Homework/F-strings-methods/exercise.js
+++ week-1/Homework/F-strings-methods/exercise.js
@@ -1,3 +1,4 @@
 // Start by creating a variable `message`
-
+const name = "Noura"
+const message = "My name is " + name +  " and my name is " + name.length + " characters long";
 console.log(message);
